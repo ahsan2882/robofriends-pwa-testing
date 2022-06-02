0698c05345b186cf8a82a847dcb7c1b83b3ee3f6
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-06-02 13:52:49 +0500

    Update accessibility score

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ function Card({ id, name, email }) {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img
-        src={`https://robohash.org/${id}?200x200`}
+        src={`https://robohash.org/${id}?size=200x200`}
         width="200"
         height="200"
         alt="robot"
